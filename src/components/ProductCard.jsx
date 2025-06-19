bc50920ec70c2886a8ebf4dbe3f059774e51ddc6
Michael <michaelbrandmeyer99@gmail.com>
2025-06-18 17:26:27 -0700

    Styling

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,9 +9,17 @@ function ProductCard({title, price, img}) {
     <div className="card-top">
       <img src={img} alt={title} />
     </div>
-    <div className="card-bottom">
-      <p className="card-title">{title}</p>
+    <div className="card-middle">
+      <p className="card-title two-line-text">{title}</p>
       <p className="card-price">${price}</p>
+    </div>
+    <div className="card-bottom">
+      <div className="qty">
+        <button className="decrease-quantity">-</button>
+        <input type="number" value="0"/>
+        <button className="increase-quantity">+</button>
+      </div>
+      <button className = "addItemButton">Add To Cart</button>
     </div>
   </div>
   );
